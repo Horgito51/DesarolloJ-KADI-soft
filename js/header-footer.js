da57d68a5ea90fa4161a34b4e11afae7b2573f1a
Horgito51 <jorgeandrejara15@gmail.com>
2025-11-12 09:43:34 -0500

    commit

--- js/header-footer.js
+++ js/header-footer.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
           <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="none" viewBox="0 0 24 24" stroke="white" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
           </svg>
-          <p style="color:white; margin-top: 5px; font-size: 14px; font-weight: bold;"><b>Carrito</b></p>
+          <p style="color:white; margin-top: 5px; font-size: 14px; font-weight: bold; font-size:1vw"><b>Carrito</b></p>
         </a>
       </div>
 
@@ -42,7 +42,7 @@ $(document).ready(function() {
             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
             <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
           </svg>
-          <p style="text-align: center; color:white; margin-top: 5px;"><b>Iniciar sesión</b></p>
+          <p style="text-align: center;  font-weight: bold; color:white; margin-top: 5px; font-size:1vw"><b>Iniciar sesión</b></p>
         </a>
       </div>
     </div>
