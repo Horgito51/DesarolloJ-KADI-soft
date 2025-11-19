230f6bf989b77754198f449d7501532c4e9fd962
Horgito51 <jorgeandrejara15@gmail.com>
2025-11-19 10:10:13 -0500

    Header MVC

---
+++ mvc/controllers/headerController.js
@@ -0,0 +1,14 @@
+import headerModel from "../models/headerModel.js";
+import headerView from "../views/headerView.js";
+export default class headerController{
+  constructor(){
+    this.Modelheader=new headerModel();
+    this.Viewheader=new headerView();
+  }
+  init(){
+    console.log("llega");
+     this.Viewheader.renderHeader();
+  }
+  }
+
+
